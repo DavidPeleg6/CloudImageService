88bdaa8d7f2eb9e318b806cac3cfa12964d2ff68
AvihaiDidi <33961763+AvihaiDidi@users.noreply.github.com>
2018-04-01 17:35:14 +0300

    actually still deleting crap after all

---
+++ ImageService/ImageService.Infrastructure/Enums/CommandEnum.cs
@@ -0,0 +1,31 @@
+<<<<<<< HEAD
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.Infrastructure.Enums
+{
+    public enum CommandEnum : int
+    {
+        NewFileCommand,
+        CloseCommand
+    }
+}
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.Infrastructure.Enums
+{
+    public enum CommandEnum : int
+    {
+        NewFileCommand,
+        CloseCommand
+    }
+}
+>>>>>>> 8df13f159e3620029bc1b1107583aee262ed79a4
---
+++ ImageService/ImageService.Logging/LoggingService.cs
@@ -0,0 +1,26 @@
+﻿
+using ImageService.Logging.Modal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.Logging
+{
+    public class LoggingService : ILoggingService
+    {
+        public event EventHandler<MessageRecievedEventArgs> MessageRecieved; //this event happens when you get a messege
+        public void Log(string message, MessageTypeEnum type)
+        {
+            EventHandler<MessageRecievedEventArgs> handler = MessageRecieved;
+            if (handler != null)
+            {
+                MessageRecievedEventArgs args = new MessageRecievedEventArgs();
+                args.Status = type;
+                args.Message = message;
+                handler(this, args);
+            }
+        }
+    }
+}
