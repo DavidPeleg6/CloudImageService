aaf808a2f0bbad5f41624b56f5a37bd244d2d476
dudugeda <dudugeda@gmail.com>
2018-04-01 18:31:43 +0300

    Add files via upload
    image modal

--- ImageService/ImageService/Modal/IImageServiceModal.cs
+++ ImageService/ImageService/Modal/IImageServiceModal.cs
@@ -14,5 +14,14 @@ namespace ImageService.Modal
         /// <param name="path">The Path of the Image from the file</param>
         /// <returns>Indication if the Addition Was Successful</returns>
         string AddFile(string path, out bool result);
+
+        /// <summary>
+        /// the function adds a folder
+        /// </summary>
+        /// <param name="path">The Path of the Image from the file</param>
+        /// <param name="result">Indication if the Addition Was Successful</param>
+        /// <returns>Indication if the Addition Was Successful</returns>
+        bool AddFolder(string path);
+
     }
 }
--- ImageService/ImageService/Modal/ImageServiceModal.cs
+++ ImageService/ImageService/Modal/ImageServiceModal.cs
@@ -1,12 +1,64 @@
-﻿namespace ImageService.Modal
+﻿using ImageService.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImageService.Modal
 {
     public class ImageServiceModal : IImageServiceModal
     {
-        #region Members
-        private string m_OutputFolder;            // The Output Folder
-        private int m_thumbnailSize;              // The Size Of The Thumbnail Size
-       
-        #endregion
+        private static Regex r = new Regex(":");
 
+        public string AddFile(string path, out bool result)
+        {
+            //first check if file exists
+            if(System.IO.File.Exists(path))
+            {
+                result = false;
+                return "fail";
+            }
+            // top-level folder name.
+            string sourcePath = @"c:\OutputDir";
+            DateTime date = GetDateTakenFromImage(path);
+            //parse date into path for a new folder
+            string time = date.Year.ToString() + @"\" + date.Month.ToString();
+            sourcePath = System.IO.Path.Combine(sourcePath, time);
+            AddFolder(sourcePath);
+            //get file name to update path for copying
+            string fileName = System.IO.Path.GetFileName(path);
+            string targetFile = System.IO.Path.Combine(sourcePath, fileName);
+            System.IO.File.Copy(path, targetFile);
+            result = true;
+            return "success";
+        }
+
+        public bool AddFolder(string path)
+        {
+            if (!System.IO.Directory.Exists(path))
+            {
+                System.IO.Directory.CreateDirectory(path);
+                return true;
+            }
+            return false;
+        }
+
+        //retrieves the datetime WITHOUT loading the whole image
+        public static DateTime GetDateTakenFromImage(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (Image myImage = Image.FromStream(fs, false, false))
+            {
+                PropertyItem propItem = myImage.GetPropertyItem(36867);
+                string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                return DateTime.Parse(dateTaken);
+            }
+        }
     }
 }
