1a22e824c7ef5b618c9d9e967e172b34aa30ec9b
AvihaiDidi <33961763+AvihaiDidi@users.noreply.github.com>
2018-04-01 17:51:13 +0300

    maybe now it'll actually not be full of crap

--- ImageService/ImageService.Infrastructure/Enums/CommandEnum.cs
+++ ImageService/ImageService.Infrastructure/Enums/CommandEnum.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,19 +12,3 @@ namespace ImageService.Infrastructure.Enums
         CloseCommand
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ImageService.Infrastructure.Enums
-{
-    public enum CommandEnum : int
-    {
-        NewFileCommand,
-        CloseCommand
-    }
-}
->>>>>>> 8df13f159e3620029bc1b1107583aee262ed79a4
--- ImageService/ImageService/Modal/ImageServiceModal.cs
+++ ImageService/ImageService/Modal/ImageServiceModal.cs
@@ -1,16 +1,4 @@
-﻿using ImageService.Infrastructure;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace ImageService.Modal
+﻿namespace ImageService.Modal
 {
     public class ImageServiceModal : IImageServiceModal
     {
