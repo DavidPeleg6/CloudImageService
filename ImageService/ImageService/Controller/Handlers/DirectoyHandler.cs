a7e26f5306c9ae5e36cced763737862180fc1e1f
dudugeda <dudugeda@gmail.com>
2018-04-01 22:49:15 +0300

    Add files via upload
    directory handler not complete

--- ImageService/ImageService/Controller/Handlers/DirectoyHandler.cs
+++ ImageService/ImageService/Controller/Handlers/DirectoyHandler.cs
@@ -10,6 +10,9 @@ using ImageService.Infrastructure.Enums;
 using ImageService.Logging;
 using ImageService.Logging.Modal;
 using System.Text.RegularExpressions;
+using System.Security.Permissions;
+using ImageService.Commands;
+
 
 namespace ImageService.Controller.Handlers
 {
@@ -24,6 +27,42 @@ namespace ImageService.Controller.Handlers
 
         public event EventHandler<DirectoryCloseEventArgs> DirectoryClose;              // The Event That Notifies that the Directory is being closed
 
-		// Implement Here!
+        public DirectoyHandler(ILoggingService logger)
+        {
+            this.m_logging = logger;
+            this.m_controller = new ImageController(new ImageServiceModal());
+        }
+        //starts the file system watcher
+        //returns if directory doesnt exists
+        public void StartHandleDirectory(string dirPath)
+        {
+            this.m_path = dirPath;
+            if (!System.IO.Directory.Exists(dirPath))
+            {
+                return;
+            }
+            //set the watcher an filter appropriate types
+            m_dirWatcher = new FileSystemWatcher(dirPath, "*.jpg");
+            m_dirWatcher.Filter = "*.gif";
+            m_dirWatcher.Filter = "*.bmp";
+            m_dirWatcher.Filter = "*.png";
+            m_dirWatcher.Changed += new FileSystemEventHandler(OnCreated);
+            m_dirWatcher.EnableRaisingEvents = true;
+        }
+
+        private void OnCreated(object source, FileSystemEventArgs e)
+        {
+            bool result;
+            string[] args = new string[1];
+            args[0] = e.FullPath;
+            string msg = this.m_controller.ExecuteCommand(0, args, out result);
+            if(result)
+            {
+                this.m_logging.Log(msg, (MessageTypeEnum)0);
+            } else
+            {
+                this.m_logging.Log(msg, (MessageTypeEnum)2);
+            }
+        }
     }
 }
