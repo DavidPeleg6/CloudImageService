b205184834d66e9388211b31dd6336dafdec6f36
AvihaiDidi <33961763+AvihaiDidi@users.noreply.github.com>
2018-04-02 13:50:17 +0300

    Added an installer and a program entry point, fixed some other small stuff too.

---
+++ ImageService/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            //TODO something probably
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new ImageService.ImageService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
